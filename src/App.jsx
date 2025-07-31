5df1b88346582e97257b156b1fe84a8f589ba972
Juan David <jdavidolmosc@gmail.com>
2025-07-31 15:05:59 -0500

    adding landing page's visuals

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,13 @@ function App() {
         <div className="landing">
           <div className="presentation">
             <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
             <p>Where Green Meets Serenity</p>
             <button onClick={() => dispatch(toggleShow())}>
               Start
             </button>
           </div>
-          <div className="aboutUS">
+          <div className="aboutUs">
             <p className="aboutUs-description">Welcome to Paradise Nursery, where green meets serenity!</p>
             <p className="aboutUs-content">
               At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of
@@ -26,14 +27,12 @@ function App() {
               more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every
               plant enthusiast.
             </p>
-            <p className="plant_logo_left"><img src="https://tse3.mm.bing.net/th/id/OIP.zeVmOtPEJDM9IFCN9DkeXQHaHk?rs=1&pid=ImgDetMain&o=7&rm=3" height='50px' width='50px' alt="" /></p>
             <p className="aboutUs-content">
               Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care.
               Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of
               the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your
               home or office.
             </p>
-            <p className="plant_logo_right"><img src="https://tse3.mm.bing.net/th/id/OIP.zeVmOtPEJDM9IFCN9DkeXQHaHk?rs=1&pid=ImgDetMain&o=7&rm=3" height='50px' width='50px' alt="" /></p>
 
             <p className="aboutUs-content">
               Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the
